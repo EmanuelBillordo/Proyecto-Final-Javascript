6adbb62c4f622a37bca56951ea0b7cacef62dff1
EmanuelBillordo <ematwit@gmail.com>
2022-08-22 18:21:04 -0300

    arreglos minimos

--- js/db.js
+++ js/db.js
@@ -3,50 +3,50 @@ var productosPapeleria = [
     {
         "tipo": "Papelería",
         "seleccion": "25 x (hojas, sobres, tarjetas, carpetas)",
-        "precio": 30000
+        "precio": 3000
     },
     {
         "tipo": "Papelería",
         "seleccion": "50 x (hojas, sobres, tarjetas, carpetas)",
-        "precio": 60000
+        "precio": 6000
     },
     {
         "tipo": "Papelería",
         "seleccion": "100 x (hojas, sobres, tarjetas, carpetas)",
-        "precio": 120000
+        "precio": 12000
     }
 ]
 var productosEmpaque = [
     {
         "tipo": "Empaque",
         "seleccion": "25 cajas",
-        "precio": 50000
+        "precio": 5000
     },
     {
         "tipo": "Empaque",
         "seleccion": "50 cajas",
-        "precio": 100000
+        "precio": 10000
     },
     {
         "tipo": "Empaque",
         "seleccion": "100 cajas",
-        "precio": 200000
+        "precio": 20000
     }
 ]
 var productosBranding = [
     {
         "tipo": "Branding",
         "seleccion": "10 x (gorras, boligrafos, libretas)",
-        "precio": 60000
+        "precio": 6000
     },
     {
         "tipo": "Branding",
         "seleccion": "20 x (gorras, boligrafos, libretas)",
-        "precio": 120000
+        "precio": 12000
     },
     {
         "tipo": "Branding",
         "seleccion": "30 x (gorras, boligrafos, libretas)",
-        "precio": 180000
+        "precio": 18000
     }
 ]
